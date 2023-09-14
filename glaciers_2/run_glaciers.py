0eddbd7d1f6389ecc1065d7bfe9741899d887a70
varyabazilova <varyabazilova@gmail.com>
2023-09-14 15:47:58 +0200

    gl_HRU working

---
+++ glaciers_2/run_glaciers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  3 16:56:57 2022
+
+@author: hirschbe
+"""
+
+from SedCas import SedCas
+
+def run():
+	model = SedCas()
+	model.load_climate()
+	model.load_params()
+	model.run_hydro()
+	model.run_sediment()
+	model.save_output()
+	model.plot_sedyield_monthly()
+
+if '__name__' == '__main__':
+	run()
